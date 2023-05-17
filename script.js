8b905a56a54512173e5e507cae9ee19d2aed0e3b
Programmika <rothe.annika@yahoo.com>
2023-05-17 17:18:37 +0200

    added JS and CSS

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-let currentPokemon;
+let loadedPokemon = 21; // so viele werden am Stück geladen
+let currentLoadedPokemon = 1;
+let listOfLoadedPokemons = [];
 
-async function loadAllPokemons() {
-  let url = "https://pokeapi.co/api/v2/pokemon/squirtle";
-  let response = await fetch(url);
-  currentPokemon = await response.json();
+async function loadPokemons() {
+  for (let i = currentLoadedPokemon; i < loadedPokemon; i++) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    let response = await fetch(url);
+    currentPokemon = await response.json();
 
-  renderPokemonInfo();
+    listOfLoadedPokemons.push(currentPokemon);
+    document.getElementById("mainContainer").innerHTML += loadPokemonHTML(
+      i,
+      currentPokemon,
+      response,
+      url
+    );
+    loadPokemonInfo(i);
+  }
 }
 
-function renderPokemonInfo() {
-  document.getElementById("pokemonName").innerHTML = currentPokemon["name"];
-  document.getElementById("pokemonImage").src =
-    currentPokemon["sprites"]["front_shiny"];
+function loadPokemonInfo(i) {
+  // Name anzeigen
+  document.getElementById(`pokemonName${i}`).innerHTML = currentPokemon["name"];
+  //Bild einfügen
+  document.getElementById(`pokemonImage${i}`).src =
+    currentPokemon["sprites"]["other"]["official-artwork"]["front_default"];
+  //Nummer hinzufügen
+  document.getElementById(`pokemonID${i}`).innerHTML += currentPokemon["id"];
+  //Typen hinzufügen
+  document.getElementById(`pokemonType${i}`).innerHTML =
+    currentPokemon["types"][0]["type"]["name"];
 }
