884e638651e58d76bc3726f7efd483336df0b2c8
Programmika <rothe.annika@yahoo.com>
2023-05-19 14:31:06 +0200

    added Css and JS for the different types

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ let loadedPokemon = 21; // so viele werden am Stück geladen
 let currentLoadedPokemon = 1;
 let listOfLoadedPokemons = [];
 
+// Funktion zum Laden von Pokémon-Daten
 async function loadPokemons() {
+  // Schleife für das Laden von Pokémon-Daten von currentLoadedPokemon bis loadedPokemon
   for (let i = currentLoadedPokemon; i < loadedPokemon; i++) {
+    // URL für die Anfrage an die PokeAPI erstellen, um Daten für das aktuelle Pokémon zu erhalten
     let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    // Auf die Antwort der Anfrage warten und speichern
     let response = await fetch(url);
+    // Die erhaltenen Daten in das currentPokemon-Objekt umwandeln
     currentPokemon = await response.json();
-
+    // Um die geladenen Pokémon-Daten in der Konsole anzuzeigen (nur so sehe ich den Pfad zu den verschiedenen Infos/Dateien )
+    console.log("Loaded Pokemon", currentPokemon);
+    // Das currentPokemon-Objekt der Liste der geladenen Pokémon hinzufügen
     listOfLoadedPokemons.push(currentPokemon);
+    // Das HTML für das geladene Pokémon generieren und zum mainContainer hinzufügen
     document.getElementById("mainContainer").innerHTML += loadPokemonHTML(
       i,
       currentPokemon,
@@ -16,18 +24,42 @@ async function loadPokemons() {
       url
     );
     loadPokemonInfo(i);
+    loadPokemonType(i, currentPokemon);
   }
 }
 
+//Funktion um die runtergeladenen Infos anzuzeigen
 function loadPokemonInfo(i) {
   // Name anzeigen
   document.getElementById(`pokemonName${i}`).innerHTML = currentPokemon["name"];
   //Bild einfügen
   document.getElementById(`pokemonImage${i}`).src =
-    currentPokemon["sprites"]["other"]["official-artwork"]["front_default"];
+    currentPokemon["sprites"]["other"]["home"]["front_default"];
   //Nummer hinzufügen
   document.getElementById(`pokemonID${i}`).innerHTML += currentPokemon["id"];
   //Typen hinzufügen
   document.getElementById(`pokemonType${i}`).innerHTML =
     currentPokemon["types"][0]["type"]["name"];
+}
+
+// Funktion um den Typ den Pokémon Typ zu laden und anzuzeigen
+function loadPokemonType(i, currentPokemon) {
+  // Schleife, um alle Typen des aktuellen Pokémon zu durchlaufen
+  for (let j = 0; j < currentPokemon["types"].length; j++) {
+    // Den Namen des aktuellen Typs des Pokémon in einer Variable speichern
+    let pokemonType = currentPokemon["types"][j]["type"]["name"];
+
+    // Das HTML für das Typsymbol des Pokémon zum Element mit der ID `typeCard${i}` hinzufügen
+    document.getElementById(`typeCard${i}`).innerHTML += `
+      <div>${fromImageToIcon(pokemonType)}</div>
+      `;
+  }
+}
+
+// Funktion zum Abrufen des HTML-Codes für das Typsymbol des Pokémon
+function fromImageToIcon(pokemonType) {
+  // Das HTML für das Typsymbol des Pokémon zurückgeben
+  return `
+<img src="img/${pokemonType}.png" class="categorieIcon"></img>
+`;
 }
--- templates.js
+++ templates.js
@@ -1,19 +1,20 @@
 function loadPokemonHTML(i) {
   return `
     <div class="mainCard" >
-            <div class="cardHeadline">
-                <h1 class="pokenumber" id="pokemonID${i}">#</h1>
-               
-                <div class="typeCard" id="typeCard${i}">
-                    <div>
-                    <div>
-                    <h1 class="description" id="pokemonName${i}">#</h1>
-                </div>
-                    
-                    <img src="" class="categorieIcon" id="pokemonType${i}">
-                    </div>
-                </div>
-            </div>
+    <div class="cardHeadline">
+    <p id="pokemonID${i}">#</p>
+    <div class="typeCard" id="typeCard${i}">
+        <div>
+        <img src="" class="categorieIcon" id="pokemonType${i}">
+        </div>
+    </div>
+  </div>
+  <div>
+  <h1 class="name" id="pokemonName${i}">Charmander</h1>
+  </div>
+                
+                
+            
             <div class="mainCardImage">
        <img   src="" id="pokemonImage${i}">
         </div>
