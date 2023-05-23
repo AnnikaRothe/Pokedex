c131e3be0646a19aaf69481bd8d08aa555e2ccc1
Programmika <rothe.annika@yahoo.com>
2023-05-23 08:22:46 +0200

    almost done

--- script.js
+++ script.js
@@ -29,14 +29,20 @@ async function loadPokemons() {
 
 //Funktion um die runtergeladenen Infos anzuzeigen
 function showPokemons(i) {
-  // Name anzeigen
-  document.getElementById(`pokemonName${i}`).innerHTML = currentPokemon["name"];
-  //Bild einfügen
+  // Name anzeigen und ersten Buchstaben in Großbuchstaben umwandeln
+  let pokemonNameElement = document.getElementById(`pokemonName${i}`);
+  let name = currentPokemon["name"];
+  let capitalizedFirstLetter = name.charAt(0).toUpperCase() + name.slice(1);
+  pokemonNameElement.innerHTML = capitalizedFirstLetter;
+
+  // Bild einfügen
   document.getElementById(`pokemonImage${i}`).src =
     currentPokemon["sprites"]["other"]["home"]["front_default"];
-  //Nummer hinzufügen
+
+  // Nummer(ID) hinzufügen
   document.getElementById(`pokemonID${i}`).innerHTML += currentPokemon["id"];
-  //Typen hinzufügen
+
+  // Typen hinzufügen
   document.getElementById(`pokemonType${i}`).innerHTML =
     currentPokemon["types"][0]["type"]["name"];
 }
@@ -64,27 +70,43 @@ function fromImageToIcon(pokemonType) {
 }
 
 function loadMorePokemons() {
-  //sponsored by ChatGPT
-  currentLoadedPokemon = loadedPokemon; // Speichert die Anzahl der bisher geladenen Pokémon
-  loadedPokemon += 21; // Erhöht die Anzahl der zu ladenden Pokémon um 21
-  loadPokemons(); // Ruft die Funktion auf, um weitere Pokémon zu laden
+  // Speichert die Anzahl der bisher geladenen Pokémon
+  currentLoadedPokemon = loadedPokemon;
+
+  // Erhöht die Anzahl der zu ladenden Pokémon um 21
+  loadedPokemon += 21;
+
+  // Ruft die Funktion auf, um weitere Pokémon zu laden
+  loadPokemons();
 
   if (!isLoading) {
     // Überprüft, ob bereits ein Ladevorgang im Gange ist
-    isLoading = true; // Setzt isLoading auf true, um anzuzeigen, dass ein Ladevorgang gestartet wurde
-    loadButton.disabled = true; // Deaktiviert den Load Button, um mehrfaches Klicken zu verhindern
+    isLoading = true;
 
-    const start = loadedCount; // Speichert den Startindex für das Laden neuer Pokémon-Karten
-    const end = Math.min(loadedCount + 3, pokemonData.length); // Berechnet den Endindex für das Laden neuer Pokémon-Karten
+    // Deaktiviert den Load Button, um mehrfaches Klicken zu verhindern
+    loadButton.disabled = true;
+
+    let start = loadedCount; // Speichert den Startindex für das Laden neuer Pokémon-Karten
+
+    // Berechnet den Endindex für das Laden neuer Pokémon-Karten
+    let end = Math.min(loadedCount + 3, pokemonData.length);
 
     for (let i = start; i < end; i++) {
-      const newCard = createPokemonCard(pokemonData[i]); // Erzeugt eine neue Pokémon-Karte
-      pokemonList.appendChild(newCard); // Fügt die neue Pokémon-Karte zur Liste hinzu
-      loadedCount++; // Erhöht den Zähler der geladenen Pokémon-Karten
+      // Erzeugt eine neue Pokémon-Karte
+      let newCard = createPokemonCard(pokemonData[i]);
+
+      // Fügt die neue Pokémon-Karte zur Liste hinzu
+      pokemonList.appendChild(newCard);
+
+      // Erhöht den Zähler der geladenen Pokémon-Karten
+      loadedCount++;
     }
 
-    isLoading = false; // Setzt isLoading wieder auf false, um anzuzeigen, dass der Ladevorgang abgeschlossen ist
-    loadButton.disabled = false; // Aktiviert den Load Button wieder
+    // Setzt isLoading wieder auf false, um anzuzeigen, dass der Ladevorgang abgeschlossen ist
+    isLoading = false;
+
+    // Aktiviert den Load Button wieder
+    loadButton.disabled = false;
   }
 }
 
@@ -126,4 +148,190 @@ function filter() {
     // Das Suchfeld ist nicht leer, daher den Button mit der ID "btn" ausblenden
     document.getElementById("btn").style.display = "none";
   }
+}
+
+function checkSecondType(currentPokemon) {
+  // Überprüfen, ob das aktuelle Pokemon zwei Typen hat
+  if (currentPokemon["types"].length === 2) {
+    // Wenn das Pokemon zwei Typen hat, HTML-Code für beide Typen generieren
+    return /*html*/ `        
+      <img src="img/${currentPokemon["types"][0]["type"]["name"]}.png" class="categorieIcon">
+      <img src="img/${currentPokemon["types"][1]["type"]["name"]}.png" class="categorieIcon">
+      `;
+  } else {
+    // Wenn das Pokemon nur einen Typ hat, HTML-Code für den einen Typ generieren
+    return /*html*/ `
+      <img src="img/${currentPokemon["types"][0]["type"]["name"]}.png" class="categorieIcon">
+      `;
+  }
+}
+
+function openOverlayCard(i) {
+  // Die Overlayay-Karte anzeigen
+  document.getElementById("cardOverlay").style.display = "flex";
+  // Das Scrollen des Body-Elements deaktivieren
+  document.body.style.overflow = "hidden";
+  // Die Margin des mainContainer-Elements auf 0 setzen
+  document.getElementById("mainContainer").style.marginTop = "0";
+
+  for (let j = 0; j < currentPokemon["types"].length; j++) {
+    let pokemonType = currentPokemon["types"][j]["type"]["name"];
+    currentPokemon = listOfLoadedPokemon[i - 1]; // Da der Index 0-basiert ist, wird `i - 1` verwendet, um den richtigen Index zu erhalten
+    // Generiere den HTML-Code für die Overlayay-Karte mit den aktuellen Pokemon-Daten
+    document.getElementById("cardOverlay").innerHTML = generateOverlayCardHTML(
+      i,
+      currentPokemon
+    );
+
+    // Entfernt die vorherige Hintergrundfarbe, bevor die neue Hintergrundfarbe hinzugefügt wird
+    let cardOverlayBack = document.getElementById("cardOverlayBack");
+    cardOverlayBack.classList.remove(
+      "fire",
+      "water",
+      "grass",
+      "electric",
+      "rock",
+      "ground",
+      "bug",
+      "poison",
+      "flying",
+      "psychic",
+      "fighting",
+      "ghost",
+      "ice",
+      "dragon",
+      "dark",
+      "steel",
+      "fairy"
+    );
+    // Füge die Hintergrundfarbe des aktuellen Pokemon-Typs zur Overlayay-Karte hinzu
+    document
+      .getElementById("cardOverlayBack")
+      .classList.add(currentPokemon["types"][0]["type"]["name"]);
+
+    let statsData = renderPokemonStats(i);
+    // Aktualisiere die Buttons der Overlayay-Karte
+    showCardButtons(i);
+
+    // Rufe die Funktion zum Rendern der Moves des aktuellen Pokemons auf
+    renderPokemonMoves(i, currentPokemon);
+  }
+
+  // Zeige die Stats des aktuellen Pokemons an
+  showStats(i);
+
+  // Fügt dem cardOverlay-Element einen Event Listener hinzu, der das Overlayay schließt, wenn darauf geklickt wird
+  document.getElementById("cardOverlay").addEventListener("click", function () {
+    // Das Overlayay ausblenden
+    document.getElementById("cardOverlay").style.display = "none";
+    // Das Scrollen des Body-Elements aktivieren
+    document.body.style.overflow = "auto";
+  });
+}
+
+function closeOverlayCard() {
+  document.getElementById("cardOverlay").style.display = "none";
+  document.body.style.overflow = "scroll";
+  document.getElementById("mainContainer").style.marginTop = "100px";
+}
+
+function showCardButtons(i) {
+  // Holt die Referenzen auf die Link- und Rechts-Buttons
+  let leftButton = document.getElementById("left");
+  let rightButton = document.getElementById("right");
+
+  // Setzt das onclick-Attribut des linken Buttons
+  leftButton.setAttribute(
+    "onclick",
+    `openOverlayCard(${i === 1 ? listOfLoadedPokemon.length : i - 1})`
+  );
+
+  // Setzt das onclick-Attribut des rechten Buttons
+  rightButton.setAttribute(
+    "onclick",
+    `openOverlayCard(${i >= listOfLoadedPokemon.length ? 1 : i + 1})`
+  );
+}
+function renderPokemonMoves(i, currentPokemon) {
+  // Definieren der aktuellen Sektion, in diesem Fall "moves"
+  let currentSection = "moves";
+
+  // Zugriff auf das Moves-Container-Element
+  let movesContainer = document.getElementById("sectionCards");
+
+  // Leeren des Moves-Containers, indem der Inhalt gelöscht wird
+  movesContainer.innerHTML = "";
+
+  // Erstellen eines Container-Div-Elements für die Moves
+  let moveDivContainer = document.createElement("div");
+
+  // Hinzufügen der CSS-Klasse "moveContainer" zum Container-Div-Element
+  moveDivContainer.classList.add("moveContainer");
+
+  // Schleife zum Durchlaufen der Moves des aktuellen Pokemons
+  for (let k = 0; k < listOfLoadedPokemon[i - 1]["moves"].length; k++) {
+    // Extrahieren des Namens des aktuellen Moves
+    let move = listOfLoadedPokemon[i - 1]["moves"][k]["move"]["name"];
+
+    // Hinzufügen des HTML-Codes für eine Move-Karte zum Container-Div-Element
+    moveDivContainer.innerHTML += `
+      <div class="moveCard" id="moveCard">
+      <div class="move">${move}</div>
+      </div>        
+      `;
+  }
+
+  // Hinzufügen des Container-Div-Elements zum Moves-Container
+  movesContainer.appendChild(moveDivContainer);
+}
+
+function renderPokemonAbility(i, currentPokemon) {
+  // Definieren der aktuellen Sektion, in diesem Fall "ability"
+  let currentSection = "ability";
+
+  // Zugriff auf das Ability-Container-Element
+  let abilityContainer = document.getElementById("sectionCards");
+
+  // Leeren des Inhalts des Ability-Containers
+  abilityContainer.innerHTML = "";
+
+  // Erstellen eines Container-Div-Elements für die Abilities
+  let abilityDivContainer = document.createElement("div");
+
+  // Hinzufügen der CSS-Klasse "abilityContainer" zum Container-Div-Element
+  abilityDivContainer.classList.add("abilityContainer");
+
+  // Schleife zum Durchlaufen der Abilities des aktuellen Pokemons
+  for (let j = 0; j < listOfLoadedPokemon[i - 1]["abilities"].length; j++) {
+    // Extrahieren des Namens der aktuellen Ability
+    let ability = listOfLoadedPokemon[i - 1]["abilities"][j]["ability"]["name"];
+
+    // Hinzufügen des HTML-Codes für eine Ability-Karte zum Container-Div-Element
+    abilityDivContainer.innerHTML += `
+      <div class="abilityCard" id="moveCard">
+      <div class="ability">${ability}</div>
+      </div>
+      
+      `;
+  }
+
+  // Hinzufügen des Container-Div-Elements zum Ability-Container
+  abilityContainer.appendChild(abilityDivContainer);
+}
+
+function renderPokemonStats(i) {
+  // Ein leeres Array, um die Statistikdaten zu speichern
+  let datas = [];
+  // Überprüfung, ob das Pokemon Statistikdaten hat
+  if (listOfLoadedPokemon[i - 1]["stats"].length > 0) {
+    // Schleife zum Durchlaufen der Statistikdaten des Pokemons
+    for (let j = 0; j < listOfLoadedPokemon[i - 1]["stats"].length; j++) {
+      // Extrahieren der Basisstatistik für den aktuellen Statistikwert
+      let stats = listOfLoadedPokemon[i - 1].stats[j].base_stat;
+      // Hinzufügen der Basisstatistik zum `datas`-Array
+      datas.push(stats);
+    }
+  }
+  // Rückgabe des `datas`-Arrays mit den Statistikdaten
+  return datas;
 }
--- templates.js
+++ templates.js
@@ -1,5 +1,7 @@
 function loadPokemonHTML(i) {
   return `
+  
+  <div class="card2" >
     <div class="mainCard" >
     <div class="cardHeadline">
     <p id="pokemonID${i}">#</p>
@@ -10,16 +12,85 @@ function loadPokemonHTML(i) {
     </div>
   </div>
   <div>
-  <h1 class="name" id="pokemonName${i}">Charmander</h1>
+  <h1 class="name" id="pokemonName${i}">#</h1>
   </div>
                 
                 
             
             <div class="mainCardImage">
-       <img   src="" id="pokemonImage${i}">
+       <img   src="" id="pokemonImage${i}" onclick="openOverlayCard(${i})">
         </div>
         </div>
 
        
     `;
+}
+
+function generateOverlayCardHTML(i, currentPokemon) {
+  return `
+  <div><img class="x" src="img/x.png" alt="exit" onclick="closeOverlayCard()"></div>
+      
+  <div><img class="left" id="left" src="img/left.png" alt="previousImg"></div>
+  <div class="BigCard">
+<div class="cardOverlay front">            
+  
+  <div class="cardHeadline">
+      <p class="pokeNumber" id="">#${currentPokemon["id"]}</p>        
+      <div class="typeCard" id="typeCard${i}">
+          <div style="display:flex">
+          ${checkSecondType(currentPokemon)} 
+          </div>
+      </div>
+  </div>
+  <div class="cardImgOverlay"><img src="${
+    currentPokemon["sprites"]["other"]["home"]["front_default"]
+  }"></div>
+  <div>
+      <h1 class="descriptionOverlay" id="">${currentPokemon["name"]}</h1>      
+  </div>
+</div>
+
+<div class="cardOverlay back" id="cardOverlayBack" >            
+  
+  <div class="cardHeadline">
+      <h1 class="pokeNumber" id="">#${currentPokemon["id"]}</h1>        
+      <div class="typeCard" id="typeCard${i}">
+          <div style="display:flex">
+          ${checkSecondType(currentPokemon)} 
+          </div>
+      </div>
+  </div>
+  
+  <div class="cardImgOverlay cardImgBack ">
+  <div class="pokemonWeight"><h3>Weight:</h3><div><h3>${
+    currentPokemon["weight"] / 10 // durch 10 teilen, da Ergebnis zu hoch
+  } kg</h3></div>
+  </div>
+  <img class="imgBack" src="${
+    currentPokemon["sprites"]["other"]["home"]["front_default"]
+  }">
+  <div class="pokemonHeight"><h3>Height:</h3><div><h3>${
+    currentPokemon["height"] / 10
+  } m</h3></div>
+  </div>
+  </div>
+  
+  <div>
+      <h1 class="description descriptionBack" id="">${
+        currentPokemon["name"]
+      }</h1>        
+  </div>
+  <div class="sections">
+      <div onclick="renderPokemonMoves(${i})" id="movesSection"><h3>Moves</h3></div>
+      <div onclick="renderPokemonAbility(${i})" id="abilitySection"><h3>Abilities</h3></div>
+  </div>
+
+  <div class="sectionCards" id="sectionCards">
+      
+       </div>          
+   </div>
+</div>
+</div>
+<div><img class="right" id="right" src="img/right.png" alt="nextImg"></div>
+  `;
 }
